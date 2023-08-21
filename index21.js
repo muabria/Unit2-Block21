65d11c520994b6dce48d803f00f7089953dd105e
briaar <briarusss@gmail.com>
2023-08-21 15:40:43 -0400

    Block21 workshop complete

--- index21.js
+++ index21.js
@@ -9,4 +9,19 @@ class Car {
         return `This Car has a $(this.make) setup, modeled by $(this.model) and created in $(this.year)`;
 
     }
+}   console.log(getDescription)
+
+class ElectricCar extends Car {
+    constructor(make, model,year, range){
+    super(make, model, year);
+    this.range = 300;
+    this.make = "Tesla";
+    this.model = "Model S"
+    this.year = "Year 2019"
+    }
+    getDescription() { 
+        return `This Car has a $(this.make) setup, modeled by $(this.model) and created in $(this.year) with a range of $(this.range)`;
+
+    }
 }
+console.log(getDescription)
