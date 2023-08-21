3e4e7183bd105262d5aa289db66da471d4895a3d
briaar <briarusss@gmail.com>
2023-08-21 16:18:02 -0400

    pseudocode to understand better

--- index21.js
+++ index21.js
@@ -1,27 +1,43 @@
-
+// Define Car and constructor - make, model, year 
+//have a Return with information on car
 class Car { 
-    constructor(make, model,year){
+    constructor(make, model, year){
         this.make = make;
         this.model = model; 
         this.year = year;
     }
     getDescription() {
-        return `This Car has a $(this.make) setup, modeled by $(this.model) and created in $(this.year)`;
+        return `This Car has a $(this.make) setup, 
+        modeled by $(this.model),
+        created in $(this.year)`;
 
     }
-}   
+}; 
 
+
+//ElectricCar is subclass
+//ADD this.range = range
 class ElectricCar extends Car {
-    constructor(make, model,year, range){
+    constructor(make, model, year, range){
     super(make, model, year);
-    this.range = 300;
-    this.make = "Tesla";
-    this.model = "Model S"
-    this.year = "Year 2019"
+    this.range = range;
     }
+
+//ElectricCar prototype should ovverride the getDescription method w/ NEW implementations
+//override return with '$(super.)
+
     getDescription() { 
-        return `This Car has a $(this.make) setup, modeled by $(this.model) and created in $(this.year) with a range of $(this.range)`;
+        return `${super.getDescription()} 
+        This Car also has a range of $(this.range)`;
 
     }
-}
+};
 
+// Create an instance of ElectricCar
+// add in Make: Tesla, Model: Model S, Year: 2019, and Range: 300
+// Output should be a string containing the model, make, year, and range of ElectricCar.
+
+const electricModel = new ElectricCar("Tesla", "Model S", "Year 2019", 300);
+
+const getDescription = electricModel.getDescription();
+console.log(getDescription);
